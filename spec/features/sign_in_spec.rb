7958ad38d0858872871d366379f1d51ac7c0e94b
Mark Johnson <mjohnson@mellowcoder.com>
2014-05-27 16:15:38 -0400

    Refactored Sign in feature into a macro

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,11 +1,10 @@
+require 'spec_helper'
+
 feature "Sign in" do
   given(:steve) {Fabricate(:user, email: "smartin@example.com", password: "king-tut", full_name: "Steve Martin")}
 
   scenario "with valid credentials" do
-    visit sign_in_path
-    fill_in 'Email Address', with: steve.email
-    fill_in 'Password', with: steve.password
-    click_button 'Sign in'
+    feature_sign_in(steve)
     expect(page).to have_content steve.full_name
   end
   
