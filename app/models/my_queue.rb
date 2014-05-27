77a8a3146730f6511068014a5204ad302ede1c8a
Mark Johnson <mjohnson@mellowcoder.com>
2014-05-27 16:57:53 -0400

    Refactoring interaction with the users queue to use the new MyQueue object

--- app/models/my_queue.rb
+++ app/models/my_queue.rb
@@ -1,15 +1,44 @@
 class MyQueue
     
+  attr_reader :owner
+  
   def initialize(owner)
     @owner = owner
   end
   
   def items
-    @items ||= @owner.queue_items
+    @items ||= owner.queue_items
   end
   
   def video_exists_in_queue?(video)
     items.map(&:video).include?(video)
+  end
+  
+  
+  def add_video(video)
+    QueueItem.create(user: owner, video: video, position: new_queue_item_position) unless video_exists_in_queue?(video)
+  end
+  
+  def remove_item(id)
+    item = items.where(id: id).first
+    item.destroy && normalize_queue_item_positions if item
+  end
+  
+  def update_items(items)
+    ActiveRecord::Base.transaction do
+      items.each_pair do |key, item_data|
+        item = QueueItem.find(key)
+        item.update_attributes!(position: item_data["position"], rating: item_data["rating"]) if item.user == owner
+      end
+        normalize_queue_item_positions
+    end
+  end
+  
+  
+  private
+  
+  def new_queue_item_position
+    items.count+1
   end
   
   def normalize_queue_item_positions
--- spec/models/my_queue_spec.rb
+++ spec/models/my_queue_spec.rb
@@ -23,15 +23,5 @@ describe MyQueue do
       expect(user.queue.video_exists_in_queue?(other_video)).to eq(false)
     end
   end
-  
-  describe "normalize_queue_item_positions" do
-    it "renumbers the queue items in sequental sequence" do
-      Fabricate(:queue_item, user: user, video: video, position: 5)
-      Fabricate(:queue_item, user: user, video: video, position: 6)
-      user.queue.normalize_queue_item_positions
-      expect(user.queue.items.map(&:position)).to eq([1,2])
-    end
-  end
-  
 
 end
