c09abb1811ecee6ff8832c4a5ca847819e599107
Mark Johnson <mjohnson@mellowcoder.com>
2014-06-03 18:31:17 -0400

    Added Tokened Concern

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -2,12 +2,6 @@ class Invite < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :email, :name, :message, :user
   
-  before_create :generate_token
-  
-  private 
-  
-  def generate_token
-    self.token = SecureRandom.urlsafe_base64
-  end
-  
+  include Tokened
+
 end
