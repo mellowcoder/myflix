1bf19da667a7cac194cce843d8dde78487551b5e
Mark Johnson <mjohnson@mellowcoder.com>
2014-06-09 11:24:27 -0400

    Added support for uploading and playing videos

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -4,4 +4,19 @@ class Admin::VideosController < Admin::AdministrationController
     @video = Video.new
   end
     
+  def create
+    @video = Video.new(video_params)
+    if @video.save
+      flash[:success] = "Video was successfully saved"
+      redirect_to new_admin_video_path
+    else
+      flash[:error] = "The was an error with saving the video"
+      render :new
+    end
+  end
+  
+  def video_params
+    params.require(:video).permit(:title, :description, :category_id, :large_cover, :small_cover)
+  end
+  
 end
